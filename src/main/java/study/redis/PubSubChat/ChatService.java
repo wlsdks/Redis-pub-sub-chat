aad4c36d04ebdedefe2d17fcd77030e1e68804ca
jinan <wlsdks12@naver.com>
2023-06-21 09:48:16 +0900

    1. 커밋추가

--- src/main/java/study/redis/PubSubChat/ChatService.java
+++ src/main/java/study/redis/PubSubChat/ChatService.java
@@ -3,6 +3,7 @@ package study.redis.PubSubChat;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Service;
@@ -15,17 +16,25 @@ public class ChatService implements MessageListener {
 
     private final RedisMessageListenerContainer container;
 
+    private final RedisTemplate<String, String> redisTemplate;
+
     public void enterChatRoom(String chatRoomName) {
         container.addMessageListener(this, new ChannelTopic(chatRoomName));
 
         Scanner in = new Scanner(System.in);
         while (in.hasNextLine()) {
             String line = in.nextLine();
+            // 사용자가 q를 입력하면 종료된다.
             if (line.equals("q")) {
                 System.out.println("Quit...");
                 break;
             }
+
+            // line을 보내주면 사용자가 입력할때마다 topic으로 메시지를 전송한다.
+            redisTemplate.convertAndSend(chatRoomName, line);
         }
+
+        container.removeMessageListener(this);
     }
 
     @Override
