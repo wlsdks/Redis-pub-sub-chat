efbb4e21e257326a86f1298860fe2ad6a8418057
jinan <wlsdks12@naver.com>
2023-06-21 09:45:18 +0900

    1. 커밋

--- src/main/java/study/redis/PubSubChat/ChatService.java
+++ src/main/java/study/redis/PubSubChat/ChatService.java
@@ -1,13 +1,35 @@
 package study.redis.PubSubChat;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Service;
 
+import java.util.Scanner;
+
 @Service
+@RequiredArgsConstructor
 public class ChatService implements MessageListener {
+
+    private final RedisMessageListenerContainer container;
+
+    public void enterChatRoom(String chatRoomName) {
+        container.addMessageListener(this, new ChannelTopic(chatRoomName));
+
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            if (line.equals("q")) {
+                System.out.println("Quit...");
+                break;
+            }
+        }
+    }
+
     @Override
     public void onMessage(Message message, byte[] pattern) {
-
+        System.out.println("Message: " + message.toString());
     }
 }
--- src/main/java/study/redis/PubSubChat/PubSubChatApplication.java
+++ src/main/java/study/redis/PubSubChat/PubSubChatApplication.java
@@ -1,11 +1,15 @@
 package study.redis.PubSubChat;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class PubSubChatApplication implements CommandLineRunner {
+
+	private final ChatService chatService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PubSubChatApplication.class, args);
@@ -14,5 +18,7 @@ public class PubSubChatApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Application started..");
+
+		chatService.enterChatRoom("chat1");
 	}
 }
